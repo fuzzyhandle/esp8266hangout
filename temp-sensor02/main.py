5761364149b3171521cb4f72f591dc5f5cbd77d6
HD <hrishikesh.date@gmail.com>
2016-08-30 21:51:34 +0530

    Build a query string with params in a dictionary and append it to the URL. Makes the code readale. Remove commented code

--- temp-sensor02/main.py
+++ temp-sensor02/main.py
@@ -2,20 +2,23 @@ from machine import Pin
 from ds18x20 import DS18X20
 import onewire
 import time
-import machine
 import ujson
 import urequests
 
 def posttocloud(temperature):
+  
   keystext = open("sparkfun_keys.json").read()
   keys = ujson.loads(keystext)
-  url = keys['inputUrl'] + "?private_key=" + keys['privateKey'] + "&temp=" + str(temperature)
-  #data = {'temp':temperature}
-  #data['private_key'] = keys['privateKey']
-  #print (keys['inputUrl'])
-  #print(keys['privateKey'])
-  #datajson = ujson.dumps(data)
-  #print (datajson)
+  params = {}
+  params['temp'] = temperature
+  params['private_key'] = keys['privateKey']
+  
+  #data.sparkfun doesn't support putting data into the POST Body.
+  #We had to add the data to the query string
+  #Copied the Dirty hack from
+  #https://github.com/matze/python-phant/blob/24edb12a449b87700a4f736e43a5415b1d021823/phant/__init__.py
+  payload_str = "&".join("%s=%s" % (k, v) for k, v in params.items())
+  url = keys['inputUrl'] + "?" + payload_str
   resp = urequests.request("POST", url)
   print (resp.text)
 
