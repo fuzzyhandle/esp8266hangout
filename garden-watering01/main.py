d89837e93f4f2fa346a5f3f619cf1f77790da79f
HD <hrishikesh.date@gmail.com>
2016-09-05 01:07:40 +0530

    Create dashboard and feeds on adafruit Add logic to update the feeds

--- garden-watering01/main.py
+++ garden-watering01/main.py
@@ -1,13 +1,14 @@
+import mybuddy
+
 import machine
-from ds18x20 import DS18X20
-import onewire
 import time
 import ujson
 import urequests
-# ntptime is a helper module which gets packaged into the firmware
-# Check https://raw.githubusercontent.com/micropython/micropython/master/esp8266/scripts/ntptime.py
-import ntptime
-import pickle
+
+
+#Can be any value.
+#Using GUID to rule out duplicates
+MQTT_CLIENT_ID = "578ee6a0-d0ad-40be-8687-162a6d6136e4"
 
 #Safetynet during development
 #Give enough time to delete file before execution
@@ -15,33 +16,6 @@ for x in range (3):
   print (".",end='')
   time.sleep (1)
 print ("")
-
-
-# class RTCUserData:
-  
-  # def __init__(self):
-    # self._ntptime = 0
-    # self._lastwateringtime = 0
-    
-  # @property
-  # def ntptime(self):
-      # print("Getting value")
-      # return self._ntptime
-
-  # @ntptime.setter
-  # def ntptime(self, value):
-    # print("Setting value")
-    # self._ntptime = value
-
-  # @property
-  # def lastwateringtime(self):
-      # print("Getting value")
-      # return self._lastwateringtime
-
-  # @lastwateringtime.setter
-  # def lastwateringtime(self, value):
-    # print("Setting value")
-    # self._lastwateringtime = value
 
 def waternow(pulseduration=10):
   print ("Watering started")
@@ -55,51 +29,39 @@ def waternow(pulseduration=10):
   
   print ("Watering done")
   
-def deepsleep():
 
-  # configure RTC.ALARM0 to be able to wake the device
-  rtc = machine.RTC()
-  rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
-
-  # set RTC.ALARM0 to fire after some time. Time is given in milliseconds here
-  rtc.alarm(rtc.ALARM0, 30*1000)
-
-  #Make sure you have GPIO16 connected RST to wake from deepSleep.
-  # put the device to sleep
-  print ("Going into Sleep now")
-  machine.deepsleep()
-
-def get_temperature():
-  p = machine.Pin(2) # Data Line is on GPIO2 aka D4
-  ow = onewire.OneWire(p)
-  ds = DS18X20(ow)
-  lstrom = ds.scan()
-  #Assuming we have only 1 device connected
-  rom = lstrom[0]
-
-  ds.convert_temp()
-  time.sleep_ms(750)
-  temperature = round(float(ds.read_temp(rom)),1)
-  print("Temperature: {:02.1f}".format(temperature))  
-  return temperature
+def get_adafruit_config():
+  keystext = open("adafruit.json").read()
+  adafruitconfig = ujson.loads(keystext)
+  return adafruitconfig
   
-def post_temperature_tocloud(temperature):
+def get_adafriut_mqtt_client(connect=False):
+  from umqtt.simple import MQTTClient
+  adafruitconfig = get_adafruit_config()
+  c = MQTTClient(MQTT_CLIENT_ID, adafruitconfig['servername'],adafruitconfig['port'],adafruitconfig['username'],adafruitconfig['aiokey'] )
+  if connect:
+    c.connect()
+  return c
   
-  keystext = open("sparkfun_keys_temperature.json").read()
-  keys = ujson.loads(keystext)
-  params = {}
-  params['temp'] = "{:02.1f}".format(round(temperature,1))
-  params['private_key'] = keys['privateKey']
+def read_from_cloud(feedname):
+  adafruitconfig = get_adafruit_config()
+  url = "http://" + adafruitconfig['servername'] + "/api/feeds/" + feedname
+  #url = "http://io.adafruit.com/api/feeds/" + feedname
+  print (url)
+  print (adafruitconfig['aiokey'])
+  headers = {'X-AIO-Key':adafruitconfig['aiokey']}
+  resp = urequests.request("GET", url, headers=headers)
+  print (resp.content)
+  respdata = resp.json()
+  return  respdata['last_value'] 
   
-  #data.sparkfun doesn't support putting data into the POST Body.
-  #We had to add the data to the query string
-  #Copied the Dirty hack from
-  #https://github.com/matze/python-phant/blob/24edb12a449b87700a4f736e43a5415b1d021823/phant/__init__.py
-  payload_str = "&".join("%s=%s" % (k, v) for k, v in params.items())
-  url = keys['inputUrl'] + "?" + payload_str
-  resp = urequests.request("POST", url)
-  print (resp.text)
+def post_to_cloud(feedname,value):
+  adafruitconfig = get_adafruit_config()
+  client = get_adafriut_mqtt_client(connect=True)
+  client.publish('{0}/feeds/{1}'.format(adafruitconfig['username'], feedname), str(value),qos=1)
+  client.disconnect()
   
+
 if __name__ == "__main__":
   #Put things here which can be done before needing wifi
   
@@ -124,41 +86,63 @@ if __name__ == "__main__":
   import network
   wlan = network.WLAN(network.STA_IF)
   
-  while not wlan.isconnected() :
-    time.sleep(1)
-  
-  #Flow comes here only when we have wifi
-  ntptime.settime()
-  rtcdata['ntptime'] = ntptime.time()
+  wificonnected = False
+  for i in range (60):
+    if wlan.isconnected():
+      wificonnected = True
+      break
+    else:
+      time.sleep(1)
 
+  if not wificonnected:
+    print ("No Wifi. Going to sleep")
+    mybuddy.deepsleep(2*60*1000)
+
+  #Flow comes here only when we have wifi
+  try:
+    mybuddy.setntptime(10)
+  except:
+    print ("Error setting NTP Time. Going to sleep")
+    mybuddy.deepsleep(2*60*1000)
+    
+  rtcdata['ntptime'] = time.time()
+  post_to_cloud('i-am-alive', time.time())
+
+  
+  #Read data from Cloud
+  lastwateringtime = int(read_from_cloud ('last-watering-time'))
+  if lastwateringtime is not None:
+    rtcdata['lastwateringtime'] = lastwateringtime
+  
+  wateringdosageduration = int(read_from_cloud ('water-dosage-length'))
+  wateringdosageinterval = 60* 60* int(read_from_cloud ('water-dosage-frequency'))
   #No data of last watering
   #Water immediately
   if rtcdata.get('lastwateringtime',0) == 0:
-    waternow()
+    waternow(wateringdosageduration)
     rtcdata['lastwateringtime'] = time.time()
+    post_to_cloud('last-watering-time',rtcdata['lastwateringtime'])
   else:
     #Check if delta has expired
-    if rtcdata['lastwateringtime'] + 120 <= time.time():
-      waternow()
+    if rtcdata['lastwateringtime'] + wateringdosageinterval <= time.time():
+      waternow(wateringdosageduration)
       rtcdata['lastwateringtime'] = time.time()
+      post_to_cloud('last-watering-time',rtcdata['lastwateringtime'])
+
+  rtcdata['wateringdosageduration'] = wateringdosageduration
+  rtcdata['wateringdosageinterval'] = wateringdosageinterval
+  rtcdata['i-am-alive'] = time.time()
   
-  # try:
-    # temperature = get_temperature()
-    # post_temperature_tocloud(temperature)
-  # except:
-    # print "Error Posting Temperature"
+  post_to_cloud('next-watering-in', rtcdata['lastwateringtime'] + rtcdata['wateringdosageinterval'] - rtcdata['ntptime'])    
   
   if True:
     import json
     _rtc = machine.RTC()
-    #datastring = json.dumps(rtcdata)
     datastring = json.dumps(rtcdata)
     print("Saving Data in RTC %s"%(datastring))
-    #print (len(datastring))
     _rtc.memory(datastring)
-    
     time.sleep (2)
-    
   
-  deepsleep()
+  post_to_cloud('i-am-alive', rtcdata['i-am-alive'])
   
+  mybuddy.deepsleep(15*60*1000)
