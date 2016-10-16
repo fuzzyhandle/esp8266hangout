4ba2b6008fd4b5d3168d4abbb29ea5c8f7d87aa7
HD <hrishikesh.date@gmail.com>
2016-10-16 10:19:49 +0530

    Add utility script to start wifi

---
+++ garden-watering01/mywifi.py
@@ -0,0 +1,22 @@
+class mywifi:
+  @classmethod
+  def connect(cls):
+    import network
+    import ujson
+    
+    keystext = open("wifi.json").read()
+    wificonfig = ujson.loads(keystext)    
+    
+    wlan =  network.WLAN(network.STA_IF)
+    wlan.active(True)
+    
+    wlan.connect(wificonfig['ssid'], wificonfig['password'])
+  
+    for x in range (2):
+      if wlan.isconnected():
+        break
+      time.sleep(10)
+
+    if wlan.isconnected():
+      print(wlan.ifconfig())
+    return wlan.isconnected()
